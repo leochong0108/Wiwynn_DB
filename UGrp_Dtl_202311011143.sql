ca3d51558a7a0cdae33159aac0adbde4e30438c1
Leo Chong <leochong0108@gmail.com>
2023-11-01 11:47:51 +0800

    init

---
+++ UGrp_Dtl_202311011143.sql
@@ -0,0 +1,5 @@
+INSERT INTO W0GBMA30_PROD.dbo.UGrp_Dtl (UGroup_ID,UGroup_Name,Is_Sys) VALUES
+	 (N'Admin',N'系統管理者',N'1'),
+	 (N'Empty',N'未獲權限者',N'1'),
+	 (N'Manager',N'管理者',N'1'),
+	 (N'WareHouse',N'倉管人員',N'1');
---
+++ UGrp_Mst_202311011143.sql
@@ -0,0 +1,15 @@
+INSERT INTO W0GBMA30_PROD.dbo.UGrp_Mst (UGroup_ID,User_ID) VALUES
+	 (N'Admin',N'200'),
+	 (N'Admin',N'3'),
+	 (N'Admin',N'ASRS'),
+	 (N'Admin',N'bow'),
+	 (N'Admin',N'cju'),
+	 (N'Admin',N'Joshua'),
+	 (N'Admin',N'test'),
+	 (N'Admin',N'test1'),
+	 (N'Admin',N'test20230828'),
+	 (N'Empty',N'6');
+INSERT INTO W0GBMA30_PROD.dbo.UGrp_Mst (UGroup_ID,User_ID) VALUES
+	 (N'Manager',N'2'),
+	 (N'WareHouse',N'1'),
+	 (N'WareHouse',N'bowman3');
